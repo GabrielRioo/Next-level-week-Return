c074c296c59c84fc3215e5c427dde088c19e2011
GabrielRioo <gabrielpaivario@gmail.com>
2022-05-05 17:13:28 -0300

    criado scroll bar do formulario usando tailwindcss

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -37,7 +37,7 @@ export function FeedbackContentStep({
 
       <form className="my-4 w-full">
         <textarea 
-          className="min-width-[384px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none"  
+          className="min-width-[384px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"  
           placeholder="Conte com detalhes o que está acontecendo"
         />
       </form>
