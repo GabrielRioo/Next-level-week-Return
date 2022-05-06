0295fc1dd679af228725478099544095fb9ce257
GabrielRioo <gabrielpaivario@gmail.com>
2022-05-06 18:49:57 -0300

    Adicionando servidor ouvindo na porta 3333

--- web/src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
@@ -18,7 +18,7 @@ export function FeedbackTypeStep(props: FeedbackTypStepProps) {
           return (
             <button
               key={key}
-              className="bg-zinc-800 rounded-lg py-5 w-24 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+              className="bg-zinc-800 rounded-lg py-5 w-full flex md:w-24 flex-col items-center gap-2 border-2 border-transparent  hover:border-brand-500 focus:border-brand-500 focus:outline-none"
               onClick={() => props.onFeedBackTypeChanged(key as FeedbackType)}
               type="button"
             >
