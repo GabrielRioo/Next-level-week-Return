0455a278261f42331490460fb4b3a8ba051914bd
GabrielRioo <gabrielpaivario@gmail.com>
2022-05-06 20:28:38 -0300

    Adicionando API para envio de feedback.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,21 @@
+import { prisma } from './prisma';
 import express from "express";
 
 const app = express();
 
-app.get('/users', (req, res) => {
-  return res.send('Hello World!');
+app.use(express.json()); // vai na requisição e verifica se tem algum body JSON
+
+app.post('/feedbacks', async (req, res) => {
+  const {type, comment, screenshot} = req.body // desestrututação
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type: type,
+      comment: comment,
+      screenshot: screenshot,
+    }
+  })
+  return res.status(201).json({data: feedback})
 })
 
 app.listen(3333, () => {
